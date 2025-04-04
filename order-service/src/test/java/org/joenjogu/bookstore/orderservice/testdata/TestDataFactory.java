614d3dd6d27513813fa637f4ba6515c7e3423c49
joenjogu <joenjogu97@gmail.com>
2025-04-04 15:11:59 +0300

    spotless apply

--- order-service/src/test/java/org/joenjogu/bookstore/orderservice/testdata/TestDataFactory.java
+++ order-service/src/test/java/org/joenjogu/bookstore/orderservice/testdata/TestDataFactory.java
@@ -14,7 +14,8 @@ import org.joenjogu.bookstore.orderservice.domain.model.OrderItem;
 public class TestDataFactory {
 
     static final List<String> VALID_COUNTRIES = List.of("India", "Kenya", "Germany");
-    static final Set<OrderItem> VALID_ORDER_ITEMS = Set.of(new OrderItem("P100", "Plastic", BigDecimal.valueOf(34.00), 11));
+    static final Set<OrderItem> VALID_ORDER_ITEMS =
+            Set.of(new OrderItem("P100", "Plastic", BigDecimal.valueOf(34.00), 11));
     static final Set<OrderItem> INVALID_ORDER_ITEMS = Set.of(new OrderItem("", "", BigDecimal.TEN, 11));
 
     public static CreateOrderRequest createValidOrderRequest() {
