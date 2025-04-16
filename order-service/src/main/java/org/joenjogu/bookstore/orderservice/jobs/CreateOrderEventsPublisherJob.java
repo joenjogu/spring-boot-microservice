0211aea2ad1ba9d4bb007e80f0184b0be6aaac25
joenjogu <joenjogu97@gmail.com>
2025-04-16 12:40:43 +0300

    add shedlock

--- order-service/src/main/java/org/joenjogu/bookstore/orderservice/jobs/CreateOrderEventsPublisherJob.java
+++ order-service/src/main/java/org/joenjogu/bookstore/orderservice/jobs/CreateOrderEventsPublisherJob.java
@@ -1,6 +1,8 @@
 package org.joenjogu.bookstore.orderservice.jobs;
 
 import java.time.Instant;
+import net.javacrumbs.shedlock.core.LockAssert;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.joenjogu.bookstore.orderservice.domain.OrderEventsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,17 +10,19 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
-public class OrderEventsPublisherJob {
+public class CreateOrderEventsPublisherJob {
 
-    private static final Logger log = LoggerFactory.getLogger(OrderEventsPublisherJob.class);
+    private static final Logger log = LoggerFactory.getLogger(CreateOrderEventsPublisherJob.class);
     private final OrderEventsService orderEventsService;
 
-    public OrderEventsPublisherJob(OrderEventsService orderEventsService) {
+    public CreateOrderEventsPublisherJob(OrderEventsService orderEventsService) {
         this.orderEventsService = orderEventsService;
     }
 
     @Scheduled(cron = "${orders.publish-order-creation-events-cron}")
-    public void publishEvent() {
+    @SchedulerLock(name = "publishCreateOrderEvent")
+    public void publishCreateOrderEvent() {
+        LockAssert.assertLocked();
         log.info("Publishing create order event at {}", Instant.now());
         orderEventsService.publishOrderEvents();
     }
--- order-service/src/main/java/org/joenjogu/bookstore/orderservice/jobs/OrderProcessingJob.java
+++ order-service/src/main/java/org/joenjogu/bookstore/orderservice/jobs/OrderProcessingJob.java
@@ -1,6 +1,8 @@
 package org.joenjogu.bookstore.orderservice.jobs;
 
 import java.time.Instant;
+import net.javacrumbs.shedlock.core.LockAssert;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.joenjogu.bookstore.orderservice.domain.OrderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,7 +20,9 @@ public class OrderProcessingJob {
     }
 
     @Scheduled(cron = "${orders.publish-order-processing-events-cron}")
+    @SchedulerLock(name = "processNewOrders")
     public void processNewOrders() {
+        LockAssert.assertLocked();
         log.info("Processing new orders at {}", Instant.now());
         orderService.processNewOrders();
     }
