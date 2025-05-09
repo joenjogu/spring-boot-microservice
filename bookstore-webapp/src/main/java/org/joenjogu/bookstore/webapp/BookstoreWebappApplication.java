06581d4cec98f432337aa1edcdc2c8c634097e1d
joenjogu <joenjogu97@gmail.com>
2025-05-09 03:15:52 +0300

    add gateway and bookstore webapp

---
+++ bookstore-webapp/src/main/java/org/joenjogu/bookstore/webapp/BookstoreWebappApplication.java
@@ -0,0 +1,13 @@
+package org.joenjogu.bookstore.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreWebappApplication.class, args);
+	}
+
+}
